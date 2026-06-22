1ac0e815e9ba7deb95855b1edff53d8db9460ac3
0x1 <kevin.francart3@gmail.com>
2026-06-22 14:50:59 +0200

    misc

--- src/App.purs
+++ src/App.purs
@@ -1,18 +1,19 @@
 module App where
 
 import Prelude
-
 import Effect (Effect)
 import Effect.Console (logShow, log)
 import Effect.Now (now)
 
 fib :: Int -> Int
 fib 0 = 0
+
 fib 1 = 1
+
 fib n = fib (n - 1) + fib (n - 2)
 
 main :: Effect Unit
-main = do 
-    time <- now
-    log $ "Now: " <> show time
-    logShow $ fib 40
+main = do
+  time <- now
+  log $ "Now: " <> show time
+  logShow $ fib 40
