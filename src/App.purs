8da1716e35c421d0c727dd0ed0b3a5a1e534b8c1
0x1 <kevin.francart3@gmail.com>
2026-06-22 15:04:40 +0200

    misc

--- src/App.purs
+++ src/App.purs
@@ -3,7 +3,22 @@ module App where
 import Prelude
 import Effect (Effect)
 import Effect.Console (logShow, log)
-import Effect.Now (now)
+
+data Expr
+  = Val Int
+  | Add Expr Expr
+  | Mul Expr Expr
+  | Sub Expr Expr
+
+eval :: Expr -> Int
+eval (Val n) = n
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
+eval (Sub a b) = eval a - eval b
+
+buildTree :: Int -> Expr
+buildTree 0 = Val 1
+buildTree n = Add (Mul (Val n) (buildTree (n - 1))) (Sub (buildTree (n - 1)) (Val 1))
 
 fib :: Int -> Int
 fib 0 = 0
@@ -12,6 +27,8 @@ fib n = fib (n - 1) + fib (n - 2)
 
 main :: Effect Unit
 main = do
-  time <- now
-  log $ "Now: " <> show time
+  log "=== AST Evaluation ==="
+  logShow $ eval (buildTree 12)
+  
+  log "=== Fibonacci ==="
   logShow $ fib 40
