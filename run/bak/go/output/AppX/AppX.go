31b8b306af80cd47f4d02516fd84e6c5bf309eb4
0x1 <kevin.francart3@gmail.com>
2026-07-23 16:08:40 +0200

    misc

---
+++ run/bak/go/output/AppX/AppX.go
@@ -0,0 +1,22 @@
+package AppX
+
+import (
+	gopurs_runtime "gopurs/output/gopurs_runtime"
+	sync "sync"
+	pkg_Test_Church "gopurs/output/Test.Church"
+)
+
+var main gopurs_runtime.Value
+var once_main sync.Once
+func Get_main() gopurs_runtime.Value {
+	once_main.Do(func() {
+		main = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value {
+_dollar__unused_0_0 := gopurs_runtime.Apply(pkg_Test_Church.Get_describe(), gopurs_runtime.Value{})
+_ = _dollar__unused_0_0
+return gopurs_runtime.Apply(pkg_Test_Church.Get_act(), gopurs_runtime.Value{})
+})
+	})
+	return main
+}
+
+
--- run/bak/scm/output/main.ss
+++ run/bak/scm/output/main.ss
@@ -2,7 +2,7 @@
 (import (chezscheme)
         (only (purescm stack-trace) print-stack-trace)
         (only (purescm finalizers) run-finalizers)
-        (only (App lib) main))
+        (only (AppX lib) main))
 (base-exception-handler (lambda (e) (print-stack-trace e) (exit -1)))
 (collect-request-handler (lambda () (collect) (run-finalizers)))
 (main)
