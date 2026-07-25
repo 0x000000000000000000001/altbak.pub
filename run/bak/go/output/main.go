e0f53d2f755a66fb3d57c396148e5c4ad9d88b37
0x1 <kevin.francart3@gmail.com>
2026-07-25 02:31:33 +0200

    misc

--- run/bak/go/output/main.go
+++ run/bak/go/output/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 	"runtime/pprof"
-	"gopurs/output/AppX"
+	"gopurs/output/App"
 	"gopurs/output/gopurs_runtime"
 )
 
@@ -15,7 +15,7 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	gopurs_runtime.Apply(AppX.Get_main(), gopurs_runtime.Value{})
+	gopurs_runtime.Apply(App.Get_main(), gopurs_runtime.Value{})
 
 	if os.Getenv("PPROF") == "1" {
 		mf, err := os.Create("mem.prof")
