0fac298262388296bdf3e8f49afcd19724d3b721
0x1 <kevin.francart3@gmail.com>
2026-08-18 20:54:08 +0200

    misc

--- src/Bench.rs
+++ src/Bench.rs
@@ -1,16 +1,27 @@
-
+use std::rc::Rc;
+use crate::{mk_number, mk_string, UnknownType, Record_a};
 
 pub fn Bench_benchNow() -> UnknownType {
-    let start = std::time::SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(std::time::UNIX_EPOCH)
-        .expect("Time went backwards");
-    let micros = since_the_epoch.as_micros() as f64;
-    mk_number(micros)
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            let start = std::time::SystemTime::now();
+            let since_the_epoch = start
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time went backwards");
+            let micros = since_the_epoch.as_micros() as f64;
+            mk_number(micros)
+        })),
+        ..Default::default()
+    })
 }
 
 pub fn Bench_opaque(mut a0: UnknownType) -> UnknownType {
-    std::hint::black_box(a0)
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            std::hint::black_box(a0.clone())
+        })),
+        ..Default::default()
+    })
 }
 
 pub fn Bench_formatNumber(mut nObj: UnknownType) -> UnknownType {
