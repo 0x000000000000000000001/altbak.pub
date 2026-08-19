d295823e5dbf8c21f337ee083a9e15bf169f5640
0x1 <kevin.francart3@gmail.com>
2026-08-19 19:16:06 +0200

    misc

--- src/Bench.rs
+++ src/Bench.rs
@@ -2,26 +2,20 @@ use std::rc::Rc;
 use crate::{mk_number, mk_string, UnknownType, Record_a};
 
 pub fn Bench_benchNow() -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
-            let start = std::time::SystemTime::now();
-            let since_the_epoch = start
-                .duration_since(std::time::UNIX_EPOCH)
-                .expect("Time went backwards");
-            let micros = since_the_epoch.as_micros() as f64;
-            mk_number(micros)
-        })),
-        ..Default::default()
-    })
+    crate::Value::Func(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+        let start = std::time::SystemTime::now();
+        let since_the_epoch = start
+            .duration_since(std::time::UNIX_EPOCH)
+            .expect("Time went backwards");
+        let micros = since_the_epoch.as_micros() as f64;
+        mk_number(micros)
+    }))
 }
 
 pub fn Bench_opaque(mut a0: UnknownType) -> UnknownType {
-    perceus_ptr::PerceusPtr::new(Record_a {
-        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
-            std::hint::black_box(a0.clone())
-        })),
-        ..Default::default()
-    })
+    crate::Value::Func(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+        std::hint::black_box(a0.clone())
+    }))
 }
 
 pub fn Bench_formatNumber(mut n: f64) -> String {
