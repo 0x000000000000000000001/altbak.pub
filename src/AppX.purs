a511bbf22f80577756b227037be8ff3778780f8d
0x1 <kevin.francart3@gmail.com>
2026-08-21 00:26:18 +0200

    misc

--- src/AppX.purs
+++
@@ -1,8 +0,0 @@
-module AppX where
-import Prelude
-import Effect (Effect)
-import Bench (runBench)
-import Test.ArrayOps as ArrayOps
-
-main :: Effect Unit
-main = void $ runBench ArrayOps.describe ArrayOps.act
