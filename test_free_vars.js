eba2b46bd57b70975a2e2df75b156fe009e6eec5
0x1 <kevin.francart3@gmail.com>
2026-08-24 00:08:04 +0200

    misc

--- test_free_vars.js
+++
@@ -1,5 +0,0 @@
-// We'll write a node script to parse the JSON for Purs_Control_Extend and see what the AST is!
-const fs = require('fs');
-const json = JSON.parse(fs.readFileSync('output/purust_output/Purs_Control_Extend/module.json', 'utf8'));
-const ast = json.decls.find(d => d.ident === 'extendFn');
-console.log(JSON.stringify(ast, null, 2));
