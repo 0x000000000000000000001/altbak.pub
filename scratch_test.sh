4119944545078a0e0007130e8deb400d575ffbe1
0x1 <kevin.francart3@gmail.com>
2026-08-25 18:06:18 +0200

    misc

--- scratch_test.sh
+++
@@ -1,2 +0,0 @@
-cd /Users/0x1/Documents/htdocs/purescript-backend-optimizer
-cat src/PureScript/Backend/Optimizer/Monomorphize.purs | head -n 435 | tail -n 60
--- scratch_test_rec.sh
+++
@@ -1,2 +0,0 @@
-cd /Users/0x1/Documents/htdocs/purescript-backend-optimizer
-cat src/PureScript/Backend/Optimizer/Monomorphize.purs | grep -A 10 "finalDecls ="
