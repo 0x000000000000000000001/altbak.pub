98ef6a50f1b9c759a3c52e432632d91f5d4ff8e6
0x1 <kevin.francart3@gmail.com>
2026-08-25 17:09:20 +0200

    misc

--- src/Test/Tiny.purs
+++
@@ -1,9 +0,0 @@
-module Test.Tiny where
-
-import Prelude
-
-data Shape = Circle Int | Rect Int Int
-
-area :: Shape -> Int
-area (Circle r) = r * r
-area (Rect w h) = w * h
--- src/Test/Visible.purs
+++
@@ -1,6 +0,0 @@
-module Test.Visible where
-import Prelude
-myId :: forall @a. a -> a
-myId x = x
-foo :: Int -> Int
-foo = myId @Int
