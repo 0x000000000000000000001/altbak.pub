0c4b3e70920989aabefaf1ad0cfd833a0be212c6
0x1 <kevin.francart3@gmail.com>
2026-08-25 16:57:57 +0200

    misc

---
+++ scratch_test.sh
@@ -0,0 +1,2 @@
+cd /Users/0x1/Documents/htdocs/purescript-backend-optimizer
+cat src/PureScript/Backend/Optimizer/Monomorphize.purs | head -n 435 | tail -n 60
---
+++ scratch_test_rec.sh
@@ -0,0 +1,2 @@
+cd /Users/0x1/Documents/htdocs/purescript-backend-optimizer
+cat src/PureScript/Backend/Optimizer/Monomorphize.purs | grep -A 10 "finalDecls ="
