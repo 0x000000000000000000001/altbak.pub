2e7cf7081e6e514123d8e6e671e6fdf81a60a00b
0x1 <kevin.francart3@gmail.com>
2026-09-02 23:22:39 +0200

    misc

--- src/AppX.purs
+++
@@ -1,8 +0,0 @@
-module AppX where
-import Prelude
-import Effect (Effect)
-import Bench (runBench)
-import Test.Primes as Primes
-
-main :: Effect Unit
-main = void $ runBench Primes.describe Primes.act
