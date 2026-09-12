263b95301917cdccc2ec15562615642c486b9d69
0x1 <kevin.francart3@gmail.com>
2026-09-13 01:46:56 +0200

    misc

--- src/AppX.purs
+++
@@ -1,8 +0,0 @@
-module AppX where
-import Prelude
-import Effect (Effect)
-import Bench (runBench)
-import Test.AstTree as AstTree
-
-main :: Effect Unit
-main = void $ runBench AstTree.describe AstTree.act
